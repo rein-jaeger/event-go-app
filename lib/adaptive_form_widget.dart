54d2ff1727a7dddc26982feecd6dacdaaff66400
rein-jaeger <lien.actreonn@gmail.com>
2022-04-18 15:37:53 +0800

    implement for landscape

--- lib/adaptive_form_widget.dart
+++ lib/adaptive_form_widget.dart
@@ -1,12 +1,72 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AdaptiveFormWidget extends StatelessWidget {
   final Widget child;
+  final Size minSize;
+  final ThemeData landscapeTheme;
+  final ThemeData portraitTheme;
 
-  const AdaptiveFormWidget({Key? key, required this.child}) : super(key: key);
+  const AdaptiveFormWidget(
+      {Key? key,
+      required this.child,
+      this.minSize = const Size(360, 480),
+      required this.landscapeTheme,
+      required this.portraitTheme})
+      : super(key: key);
+
+  Size _getLandscapeSize(BuildContext context, BoxConstraints constraints) {
+    var density = MediaQuery.of(context).devicePixelRatio;
+
+    if (density > 1.3) {
+      return Size(
+          max(minSize.width, constraints.minWidth * 0.6),
+          max(minSize.height,
+              constraints.minWidth * 0.6 / minSize.aspectRatio));
+    }
+
+    return Size(
+        max(minSize.width, constraints.minHeight * minSize.aspectRatio * 0.8),
+        max(minSize.height, constraints.minHeight * 0.8));
+  }
+
+  Widget _forLandscape(BuildContext context, BoxConstraints constraints) {
+    var formSize = _getLandscapeSize(context, constraints);
+
+    var form = Theme(
+        data: landscapeTheme,
+        child: Container(
+          width: formSize.width,
+          height: formSize.height,
+          child: child,
+          decoration: BoxDecoration(
+              color: landscapeTheme.dialogBackgroundColor,
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: const <BoxShadow>[
+                BoxShadow(
+                    color: Color(0x40000000),
+                    offset: Offset(6, 6),
+                    blurRadius: 8,
+                    spreadRadius: 4)
+              ]),
+        ));
+
+    return Center(child: form);
+  }
+
+  Widget _forPortrait(BuildContext context, BoxConstraints constraints) {
+    return Container(child: child);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return child;
+    var media = MediaQuery.of(context);
+
+    if (media.orientation == Orientation.portrait) {
+      return LayoutBuilder(builder: _forPortrait);
+    } else {
+      return LayoutBuilder(builder: _forLandscape);
+    }
   }
 }
